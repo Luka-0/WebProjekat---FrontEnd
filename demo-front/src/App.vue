01db8ddb8d0a4aa0830eb1c08a259f63264ac5a1
Luka Petrovic <petrovicluka73@gmail.com>
2022-07-04 12:54:42 +0200

    HomeView ulepsavanje

--- demo-front/src/App.vue
+++ demo-front/src/App.vue
@@ -8,6 +8,7 @@
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Cherry+Swash:wght@700&display=swap');
 #app {
   /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   font-family: 'Ubuntu', sans-serif;
--- demo-front/src/views/HomeView.vue
+++ demo-front/src/views/HomeView.vue
@@ -1,57 +1,57 @@
 <template>
     <header-comp></header-comp>
-
-
     <div class="row">
-        <div class="homepage_header relativePosition noRepeatBackground"></div>
-    </div>
-
-    <div class="row">
-      <div class="col s12">
-        <h1>Welcome</h1>
-        <a type="button" href="/employees" class="btn">Show employee</a>
-      </div>
+        <div class="homepage_header relativePosition noRepeatBackground">
+          <div class="pos_absolte header_natpis pading5 blur">
+            <h3 class="left-align">Postoji li nešto bolje od toga da se <br> hrana sama spremi i dođe do Vas?</h3>
+            <p class="fontsize2vw">~ Naruči i uživaj ~</p>
+          </div>
+        </div>
     </div>
 
     <div class="container">
-      <div class="row">
-      <div class="col m6 s12 l6">
-        Tekst 
-      </div>
+      <div class="row margin12_0">
+        <h1 class="fontfamiliCherrySwash">Hrana po vašem ukusu</h1>
+        <div class="col s3">
+          <div class="card">
+            <div class="card-image z-depth-3">
+              <img src="../assets/pice.jpg" alt="slika_pica">
+              <span class="card-title fontsize2vw">Pice</span>
+            </div>
+          </div>
+        </div>
 
-      <div class="col m6 s12 l6">
-        Tekst
+        <div class="col s3">
+          <div class="card">
+            <div class="card-image z-depth-3">
+              <img src="../assets/burgeri.jpg" alt="slika_burgera">
+              <span class="card-title fontsize2vw">Burgeri</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s3">
+          <div class="card">
+            <div class="card-image z-depth-3">
+              <img src="../assets/slatkisi.jpg" alt="slika-slatkisa">
+              <span class="card-title fontsize2vw">Slatkiši</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s3">
+          <div class="card">
+            <div class="card-image z-depth-3">
+              <img src="../assets/sokovi.jpg" alt="slika-sokova">
+              <span class="card-title fontsize2vw">Piće</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-    <p>fksdjfsdfsd</p><br>
-
+    <div class="row">
+          <div class="kuvarSection"></div>
+      </div>
   <footer-comp></footer-comp>
 </template>
 
@@ -77,6 +77,13 @@ export default {
    background-size: 100% 100%;
 }
 
+.kuvarSection {
+   background-image: url("../assets/kuvar.png"); 
+   height: 560px; 
+   max-width: 100%;
+   background-size: 100% 100%;
+}
+
 .noRepeatBackground{
      background-repeat: no-repeat;
 }
@@ -93,6 +100,11 @@ export default {
     .homepage_header{
       background-size: 100% 50%;
     }
+
+  .header_natpis{
+    top:12%;
+    left: 3%;
+  }
 }
 
 </style>
